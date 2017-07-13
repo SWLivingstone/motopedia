d96fdacd9c458042ec5f76c9b23738a824edba64
SWLivingstone <scottwlivingstone@gmail.com>
2017-07-12 17:56:57 -0700

    Update readme

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, filter_html: true, strikethrough: true, highlight: true, quote: true, footnotes: true)
     @recent_updates = Wiki.order(updated_at: :desc).limit(5)
     @recently_created = Wiki.order(created_at: :desc).limit(5)
   end
